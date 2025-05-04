a0163fedf618cc4e6d5c48f037263b201cbce69b
alinkisakurek <alinkisakurek@gmail.com>
2025-05-04 12:27:59 +0300

    karakter-hareketi

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -4,30 +4,74 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    public float jumpForce = 2.0f;
     // run-idle gecisi icin
-    public float speed = 0.0f;
+    public float speed = 5.0f;
+    private float moveDirection;
+
+    private bool jump;
+    private bool grounded = true;
+    private bool moving;
     private Rigidbody2D r2d;
-    private Animator _animator;
-    
-    
+    private SpriteRenderer _spriteRenderer;
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     void Start()
     {
         r2d = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (r2d.velocity != Vector2.zero)
         {
-            speed = 1.0f;
+            moving = true;
         }
         else
         {
-            speed = 0.0f;
+            moving = false;
         }
-        
-        _animator.SetFloat("speed", speed);
+        r2d.velocity = new Vector2(moveDirection * speed, r2d.velocity.y);
+        if (jump == true)
+        {
+            r2d.velocity = new Vector2(r2d.velocity.x, jumpForce);
+            jump = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (grounded == true && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveDirection = -1.0f;
+                _spriteRenderer.flipX = true;
+                anim.SetFloat("speed", speed);
+            } else if (Input.GetKey(KeyCode.D))
+            {
+                moveDirection = 1.0f;
+                _spriteRenderer.flipX = false;
+                anim.SetFloat("speed", speed);
+            }
+        } else if (grounded == true)
+        {
+            moveDirection = 0.0f;
+            anim.SetFloat("speed", 0.0f);
+        }
+
+        if (grounded == true && Input.GetKey(KeyCode.W))
+        {
+            jump = true;
+            grounded = false;
+            anim.SetTrigger("jump");
+            anim.SetBool("grounded", false);
+        }
     }
 }
